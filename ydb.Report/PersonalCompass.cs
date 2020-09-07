307cf7bc48af02f7be01e08cd02a84833d4e8e49
yaodaibao <zhangshuaihua@tenrypharm.com>
2020-09-07 15:06:51 +0800

    添加进销存统计

--- ydb.Report/PersonalCompass.cs
+++ ydb.Report/PersonalCompass.cs
@@ -8,74 +8,59 @@ using System.Text;
 
 namespace ydb.Report
 {
-  public  class PersonalCompass
+    public class PersonalCompass
     {
 
         public string GetPersonPerReport(string dataString, string FormatResult, string callType)
         {
-            string result = "", temptime, rdataRow, datarows;
+            //加,连接起前面的json字符串
+            string result = "", rdataRow, datarows, yearweek,panelRow="",weekindex= ",\"FWeekIndex\":";
             List<string> dataRowList = new List<string>();
             //初始化状态
-            result = string.Format(FormatResult, callType, "False", "", "");
+            //   result = string.Format(FormatResult, callType, "\"False\"", "", "");
             //每个DataRow格式
-            //string rowcontent = "{{\"DataRow\": {{\"dataSets\":[{{\"values\":[{{ \"value\": {0}, \"label\": \"\"}},{{ \"value\": {1}, \"label\":\"\"}}],\"label\":\"\",\"config\": {2}}}],\"name\": \"{3}\",\"Index\":\"{4}\",\"value\":\"{5}\",\"Count\":\"{6}\",\"startTime\":\"{7}\",\"endTime\":\"{8}\"}}}}";
-
             string rowcontent = "{{\"dataSets\":[{{\"values\":[{{ \"value\": {0}, \"label\": \"\"}},{{ \"value\": {1}, \"label\":\"\"}}],\"label\":\"\",\"config\": {2}}}],\"name\": \"{3}\",\"Index\":\"{4}\",\"value\":\"{5}\",\"Count\":\"{6}\",\"startTime\":\"{7}\",\"endTime\":\"{8}\"}}";
-
-
-
             //dataString = "{\"FWeekIndex\":\"10\",\"AuthCode\":\"1d340262-52e0-413f-b0e7-fc6efadc2ee5\",\"EmployeeID\":\"4255873149499886263\",\"BeginDate\":\"2020-08-05\",\"EndDate\":\"2020-08-31\"}";
             try
             {
                 //查询实体
                 RouteEntity routeEntity = JsonConvert.DeserializeObject<RouteEntity>(dataString);
-
-
+                weekindex += routeEntity.FWeekIndex;
                 DateTime startTime, endTime;
-                switch (routeEntity.FWeekIndex)
-                {
-                    //上月
-                    case "-11":
-                        temptime = Common.GetMonthTime(DateTime.Now.AddMonths(-1));
-                        startTime = DateTime.Parse(temptime.Split('&')[0]);
-                        endTime = DateTime.Parse(temptime.Split('&')[1]);
-                        break;
-                    //本月
-                    case "10":
-                        temptime = Common.GetMonthTime(DateTime.Now);
-                        startTime = DateTime.Parse(temptime.Split('&')[0]);
-                        endTime = DateTime.Parse(temptime.Split('&')[1]);
-                        break;
-                    //上周
-                    case "-1":
-                        startTime = Common.GetWeekFirstDayMon(DateTime.Now.AddDays(-7));
-                        endTime = Common.GetWeekLastDaySun(DateTime.Now.AddDays(-7));
-                        break;
-                    //本周
-                    case "0":
-                        startTime = Common.GetWeekFirstDayMon(DateTime.Now);
-                        endTime = Common.GetWeekLastDaySun(DateTime.Now);
-                        break;
-                    default:
-                        throw new Exception();
-                }
-
-                for (int i = 1; i < 5; i++)
+                Tuple<DateTime, DateTime> pertime = ReportHelper.GetPerTime(routeEntity.FWeekIndex);
+                //开始时间
+                startTime = pertime.Item1;
+                //结束时间
+                endTime = pertime.Item2;
+                //5-8使用
+                yearweek = ReportHelper.GetYearWithWeeks(routeEntity.FWeekIndex);
+                //目前有些数据没有，暂时跳过
+                for (int i = 1; i < 9; i++)
                 {
                     //还没有流程跳过不处理
-                    if (i > 2)
+                    if (i == 1 || i == 2)
+                    {
+                        rdataRow = GetDataRow(i, rowcontent, routeEntity.EmployeeId, startTime.ToString("yyyy-MM-dd"), endTime.ToString("yyyy-MM-dd"), "");
+                        dataRowList.Add(rdataRow);
+                    }
+                    else if (i == 5 || i == 6)
+                    {
+                        panelRow += GetDataRow(i, rowcontent, routeEntity.EmployeeId, "", "", yearweek);
+                    }
+                    else
                     {
                         continue;
                     }
-                    rdataRow = GetDataRow(rowcontent, startTime.ToString("yyyy-MM-dd"), endTime.ToString("yyyy-MM-dd"), routeEntity.EmployeeId, i);
-                    dataRowList.Add(rdataRow);
+
+
                 }
-                datarows = string.Join(",", dataRowList.ToArray());
+                //加，拼接下面的json
+                datarows = string.Join(",", dataRowList.ToArray())+",";
                 //最后结果
-                result = string.Format(FormatResult, callType, "\"True\"", "\"\"","{\"DataRow\":["+datarows+"]}");
+                result = string.Format(FormatResult, callType, "\"True\"", "\"\"", "{\"DataRow\":[" + datarows + "{" +panelRow + weekindex +"}"+ "]}");
 
             }
-            catch (Exception err)             
+            catch (Exception err)
             {
                 result = string.Format(FormatResult, callType, "\"False\"", err.Message, "");
             }
@@ -86,26 +71,26 @@ namespace ydb.Report
         /// </summary>
         /// <param name="startTime"></param>
         /// <param name="endTime"></param>
-        /// <param name="EmployeeId"></param>
+        /// <param name="EmployeeId">团队的EmployeeId是'0001','0002'格式</param>
         /// <param name="rowContent"></param>
-        /// <param name="type">1,签到，2,拜访，3,流程，4,待定</param>
+        /// <param name="type">1,签到，2,拜访，3,流程，4,待定,5,艾夫吉夫 6,丙戊酸钠 7,待支付金额，8，奖金</param>
         /// <returns></returns>
-        public string GetDataRow(string rowContent, string startTime,string endTime,string EmployeeId,int viewType)
+        public string GetDataRow(int viewType, string rowContent, string EmployeeId, string startTime, string endTime, string yearweek)
         {
-            string sql = "",viewName="";
+            string sql = "", viewName = "", tempresult="", routeconfig;
             int total, okcount, per;
             switch (viewType)
             {
                 //1,签到
                 case 1:
                     viewName = "签到";
-                    sql = $"SELECT  ISNULL(SUM([RouteCount]),0) Total ,ISNULL(SUM([OKRouteCount]),0) OKCount FROM [yaodaibao].[dbo].[RouteView] where '{startTime}' <= FDate  and  FDate <= '{ endTime }' and FEmployeeID = {EmployeeId}";
-                    
+                    sql = $"SELECT  ISNULL(SUM([RouteCount]),0) Total ,ISNULL(SUM([OKRouteCount]),0) OKCount FROM [yaodaibao].[dbo].[RouteView] where '{startTime}' <= FDate  and  FDate <= '{ endTime }' and FEmployeeID in ({EmployeeId})";
+
                     break;
                 //2,拜访
                 case 2:
                     viewName = "拜访";
-                    sql = $"SELECT  ISNULL(SUM([CallCount]),0) Total ,ISNULL(SUM([CallCount] - [UnPlanedCallCount]),0) OKCount FROM [yaodaibao].[dbo].[Route_Call_View] where '{startTime}' <= FDate  and  FDate <= '{ endTime }' and FEmployeeID = {EmployeeId}";
+                    sql = $"SELECT  ISNULL(SUM([CallCount]),0) Total ,ISNULL(SUM([CallCount] - [UnPlanedCallCount]),0) OKCount FROM [yaodaibao].[dbo].[Route_Call_View] where '{startTime}' <= FDate  and  FDate <= '{ endTime }' and FEmployeeID in ({EmployeeId})";
                     break;
                 //3,流程
                 case 3:
@@ -116,28 +101,63 @@ namespace ydb.Report
                 case 4:
                     sql = "";
                     break;
+                //5,艾夫吉夫
+                case 5:
+                    viewName = "艾夫吉夫";                  
+                    sql = $"select SUM(FStock_IB) StockIB,SUM(FStock_IN) Total,SUM(FStock_EB) StockEB,SUM(FSaleAmount) OKCount from [yaodaibao].[dbo].[HospitalStock_Detail] where FFormmainID in (SELECT FID FROM[yaodaibao].[dbo].[HospitalStock] where(cast(FYear as nvarchar(4)) + cast(FWeekIndex as nvarchar(2))) in({yearweek}) and FEmployeeID in({EmployeeId}) and FProductID = '69d55ff7-d9d6-4f20-bcbc-b5244894f36e' )";
+                    break;
+                // 6,丙戊酸钠
+                case 6:
+                    viewName = "丙戊酸钠";
+                    sql = $"select SUM(FStock_IB) StockIB,SUM(FStock_IN) Total,SUM(FStock_EB) StockEB,SUM(FSaleAmount) OKCount from [yaodaibao].[dbo].[HospitalStock_Detail] where FFormmainID in (SELECT FID FROM[yaodaibao].[dbo].[HospitalStock] where(cast(FYear as nvarchar(4)) + cast(FWeekIndex as nvarchar(2))) in({yearweek}) and FEmployeeID in({EmployeeId})  and FProductID = '6e927e6d-b03a-4601-b4dd-2ec583a46768' )";
+                    break;
+                // 7,待支付金额
+                case 7:
+                    viewName = "待支付金额";
+                    sql = "";
+                    break;
+                //8，奖金
+                case 8:
+                    viewName = "奖金";
+                    sql = "";
+                    break;
             }
-           
 
             SQLServerHelper runner = new SQLServerHelper();
             DataTable dt = runner.ExecuteSql(sql);
             //百分比
-
-            total = int.Parse(dt.Rows[0]["Total"].ToString());
-            okcount = int.Parse(dt.Rows[0]["OKCount"].ToString());
-            if (total == 0)
+            total = int.Parse((dt.Rows[0]["Total"] ==DBNull.Value ) ? "0" : dt.Rows[0]["Total"].ToString());
+            okcount = int.Parse(dt.Rows[0]["OKCount"]== DBNull.Value ? "0": dt.Rows[0]["OKCount"].ToString());
+            if (viewType<5)
             {
-                per = 0;
+                if (total == 0)
+                {
+                    per = 0;
+                }
+                else
+                {
+                    per = okcount * 100 / total;
+                }
+                //获取配置文件
+                routeconfig = Common.GetCompassConfigFromXml("Route").Replace("Quot", "\"");
+                //DataRow数据            
+                tempresult = string.Format(rowContent, per, (100 - per), routeconfig, viewName, viewType, per + "%", total.ToString(), startTime, endTime);
             }
             else
             {
-                per = okcount * 100 / total;
+                //艾夫吉夫
+                if (viewType == 5)
+                {
+                    //加,拼后面的json
+                    tempresult = $"\"AFJFName\":\"艾夫吉夫\",\"AFJFCount\":{okcount},\"AFJFProductID\":\"69d55ff7-d9d6-4f20-bcbc-b5244894f36e\""+",";
+                }
+                //丙戊酸钠
+               else if (viewType==6)
+                {
+                    tempresult = $"\"BWSNName\":\"丙戊酸钠\",\"BWSNCount\":{okcount},\"BWSNProductID\":\"6e927e6d-b03a-4601-b4dd-2ec583a46768\"";
+                }
             }
-            //获取配置文件
-            string routeconfig = Common.GetCompassConfigFromXml("Route").Replace("Quot", "\"");
-            //DataRow数据
 
-            string tempresult = string.Format(rowContent, per, (100-per), routeconfig, viewName, viewType, per + "%", total.ToString(), startTime, endTime);
             return tempresult;
         }
     }
